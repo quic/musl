edbaa3b1c7fe4fbd90b2eb650003068bbb91ec65
Sid Manning <sidneym@quicinc.com>
2020-04-14 09:45:03 -0500

    Some cleanup.

--- src/thread/hexagon/__set_thread_area.s
+++ src/thread/hexagon/__set_thread_area.s
@@ -1,14 +1,7 @@
 .global __set_thread_area
 .type   __set_thread_area,@function
 __set_thread_area:
-// V61 POPQ errata.
-#if __HEXAGON_ARCH__ == 61
-	{ ugp = r0
-	  r0 = #0 }
-	  jumpr r31
-#else
 	{ ugp = r0
 	  r0 = #0
 	  jumpr r31 }
-#endif
 .size __set_thread_area, .-__set_thread_area
