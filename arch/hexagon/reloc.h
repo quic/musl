249e6e7836d6fdbbbf450c18bacf6606a5f9a43b
Sid Manning <sidneym@quicinc.com>
2020-04-14 16:43:01 -0500

    Remove #if 0'd code

--- arch/hexagon/reloc.h
+++ arch/hexagon/reloc.h
@@ -21,15 +21,5 @@
 #define REL_TPOFF       R_HEX_TPREL_32
 #define REL_DTPOFF      R_HEX_DTPREL_32
 
-#if 0
-// TODO - Not sure if these are needed
-#define REL_DTPOFF      R_HEX_??
-#define REL_TLSDESC     R_HEX_??
-#define REL_TPOFF_NEG   R_HEX_??
-#define REL_OFFSET      R_HEX_??
-#define REL_OFFSET32    R_HEX_??
-#define REL_SYM_OR_REL  R_HEX_??
-#endif
-
 #define CRTJMP(pc,sp) __asm__ __volatile__( \
 	"r29 = %1 ; jumpr %0" : : "r"(pc), "r"(sp) : "memory" )
