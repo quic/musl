fcc1e75e3dce7035e8e64cadd42c903f01ad07c0
Sid Manning <sidneym@quicinc.com>
2020-04-30 15:20:51 -0500

    Update semid_ds's membernames to match kernel

--- arch/hexagon/bits/sem.h
+++ arch/hexagon/bits/sem.h
@@ -1,9 +1,9 @@
 struct semid_ds {
 	struct ipc_perm sem_perm;
 	unsigned long sem_otime;
-	unsigned long __unused1;
+	unsigned long sem_otime_high;
 	unsigned long sem_ctime;
-	unsigned long __unused2;
+	unsigned long sem_ctime_high;
 	unsigned short sem_nsems;
 	long __unused3;
 	long __unused4;
