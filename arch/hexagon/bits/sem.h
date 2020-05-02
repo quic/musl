adf004d943949b298d351d29473d2f23b4a88b4a
Sid Manning <sidneym@quicinc.com>
2020-05-02 18:36:44 -0500

    Update(undo) sem.h
    Used arm's version as baseline.
    
    NOTE: For QEMU testing this requires qemu's
    target_semid to match, that structure's format is defined
    like so, (linux-user/hexagon/target_structs.h):
    struct target_semid64_ds
    {
      struct target_ipc_perm sem_perm;
      abi_ulong sem_otime_lo;
      abi_ulong sem_otime_hi;
      abi_ulong sem_ctime_lo;
      abi_ulong sem_ctime_hi;
    
      abi_ulong sem_nsems;
      abi_ulong __unused1;
      abi_ulong __unused2;
      abi_llong sem_otime;
      abi_llong sem_ctime;
    };

--- arch/hexagon/bits/sem.h
+++ arch/hexagon/bits/sem.h
@@ -1,10 +1,13 @@
 struct semid_ds {
 	struct ipc_perm sem_perm;
-	unsigned long sem_otime;
-	unsigned long sem_otime_high;
-	unsigned long sem_ctime;
-	unsigned long sem_ctime_high;
+	unsigned long __sem_otime_lo;
+	unsigned long __sem_otime_hi;
+	unsigned long __sem_ctime_lo;
+	unsigned long __sem_ctime_hi;
 	unsigned short sem_nsems;
+	char __sem_nsems_pad[sizeof(long)-sizeof(short)];
 	long __unused3;
 	long __unused4;
+	time_t sem_otime;
+	time_t sem_ctime;
 };
