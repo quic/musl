d6004315172458a4ad9251e3c6253149c8e5803c
Sid Manning <sidneym@quicinc.com>
2020-06-07 16:36:14 -0500

    update sigsetjmp

--- src/signal/hexagon/sigsetjmp.s
+++ src/signal/hexagon/sigsetjmp.s
@@ -1,34 +1,27 @@
-	.text
-	.globl	sigsetjmp
-	.balign 4
-	.type	sigsetjmp,@function
+.global sigsetjmp
+.global __sigsetjmp
+.type sigsetjmp,@function
+.type __sigsetjmp,@function
+.balign 4
 sigsetjmp:
+__sigsetjmp:
+	// if savemask is 0 sigsetjmp behaves like setjmp
 	{
 		p0 = cmp.eq(r1, #0)
+		if (p0.new) jump:t ##setjmp
+	}
+	{
+		memw(r0+#64+4) = r16  // save r16 in __ss[0]
+		memw(r0+#64)   = r31  // save linkregister in __fl
 		r16 = r0
-		memd(r29 + #-16) = r17:16
-		allocframe(#8)
 	}
+		call ##setjmp
 	{
-		if (p0) jump 1f
-		if (!p0) r2 = add(r16, #68)
-		memw(r16+#64) = r1
+		r0  = r16             // restore r0
+		r31 = memw(r16+#64)   // restore linkregister
+		r16 = memw(r16+#64+4) // restore r16 from __ss[0]
 	}
-	{
-		call pthread_sigmask
-		r1:0 = combine(#0, #2)
-	}
-1:
-	{
-		r0 = r16
-		r17:16 = memd(r29 + #0)
-		deallocframe
-	}
-	{
-		jump setjmp
-	}
-	.size	sigsetjmp, .-sigsetjmp
+.hidden __sigsetjmp_tail
+	jump ##__sigsetjmp_tail
 
-
-.weak	__sigsetjmp
-.set __sigsetjmp, sigsetjmp
+.size	sigsetjmp, .-sigsetjmp
