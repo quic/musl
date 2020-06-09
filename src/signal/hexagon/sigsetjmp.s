11a5e14d794adb6ca1434993f17b116343b5741b
Sid Manning <sidneym@quicinc.com>
2020-06-09 13:23:01 -0500

    Update sigsetjmp, use __ss[2] to avoid clobbering the signal mask.

--- src/signal/hexagon/sigsetjmp.s
+++ src/signal/hexagon/sigsetjmp.s
@@ -11,15 +11,16 @@ __sigsetjmp:
 		if (p0.new) jump:t ##setjmp
 	}
 	{
-		memw(r0+#64+4) = r16  // save r16 in __ss[0]
+		memw(r0+#64+4+8) = r16  // save r16 in __ss[2]
 		memw(r0+#64)   = r31  // save linkregister in __fl
 		r16 = r0
 	}
 		call ##setjmp
 	{
+		r1 = r0;
 		r0  = r16             // restore r0
 		r31 = memw(r16+#64)   // restore linkregister
-		r16 = memw(r16+#64+4) // restore r16 from __ss[0]
+		r16 = memw(r16+#64+4+8) // restore r16 from __ss[2]
 	}
 .hidden __sigsetjmp_tail
 	jump ##__sigsetjmp_tail
